b57645adec68240449a418e315818aced08cc13a
Andres Hortua <fandresh7@gmail.com>
2023-10-16 23:02:22 -0500

    solution finished

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,32 @@
-// importar modelo
+import { TaskModel } from '../models/task.js'
 
-export const getAllTasks = (req, res) => {
-  const tasks = // ????
-  res.json(tasks)
+export const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await TaskModel.getAllTasks()
+    res.json(tasks)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 }
 
-export const addTask = (req, res) => {
-  const task = // ????
-  res.json(task)
+export const addTask = async (req, res) => {
+  const { description } = req.body
+
+  try {
+    const task = await TaskModel.addTask({ description })
+    res.json(task)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 }
 
-export const deleteTask = (req, res) => {
-  // ????
-  res.json({ message: 'Deleted successfully' })
+export const deleteTask = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    await TaskModel.deleteTask({ id })
+    res.json({ message: 'Deleted successfully' })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 }
--- models/task.js
+++ models/task.js
@@ -15,18 +15,19 @@ let currentId = 1
  * @class
  */
 
-/**
- * export class TaskModel {
- *
- *
-  /**
+export class TaskModel {
+   /**
    * Fetches all tasks.
    * @name TaskModel.getAllTasks
    * @static
    * @returns {Array<Object>} Array of tasks.
    */
+   
+   static async getAllTasks() {
+      return tasks
+   }
 
-/**
+   /**
    * Adds a new task to the list.
    * @name TaskModel.addTask
    * @static
@@ -34,9 +35,26 @@ let currentId = 1
    * @returns {Object} The new task object.
    */
 
-/**
+   static async addTask({description}) {
+      const newTask = { id: currentId, description }
+      tasks.push(newTask)
+
+      currentId += 1
+
+      return newTask
+   }
+
+   /**
    * Deletes a task from the list by its ID.
    * @name TaskModel.deleteTask
    * @static
    * @param {number} id - The ID of the task to delete.
    */
+   
+   static async deleteTask({id}) {
+      const taskIndex = tasks.findIndex( task => task.id.toString() === id)
+      if(taskIndex === -1) return
+
+      tasks.splice(taskIndex, 1)
+   }
+}
